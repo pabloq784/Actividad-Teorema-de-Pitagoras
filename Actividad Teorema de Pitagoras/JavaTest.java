2206932b7d0f94f2e8f2b9ee8d0f2a03bbc7435a
pabloq784 <60245898+pabloq784@users.noreply.github.com>
2020-03-11 12:21:56 -0300

    Add files via upload

---
+++ Actividad Teorema de Pitagoras/JavaTest.java
@@ -0,0 +1,7 @@
+public class JavaTest {
+    public static void main(String[] args) {
+        Teorema_Pitagoras iD = new Teorema_Pitagoras();
+        double hipotenusa = iD.calculateHypotenuse(3,4);
+        System.out.println(hipotenusa);
+    }
+}
---
+++ Actividad Teorema de Pitagoras/Teorema_Pitagoras.java
@@ -0,0 +1,8 @@
+public class Teorema_Pitagoras {
+            public double calculateHypotenuse(int legA, int legB) {
+                return(Math.hypot(legA,legB));
+            //La hipotenusa es el lado opuesto del ángulo recto.
+            //Calcular el valor de c dados los valores catetoA y catetoB
+
+            }
+    }
